b2aa12fcf40ed09df4c239a4fb67bd6831a76862
shenself929 <3319537169@qq.com>
2026-06-03 21:12:40 +0800

    vault backup: 2026-06-03 21:12:40

---
+++ node_modules/@google/gemini-cli/bundle/https-proxy-agent-AVGR4LHR.js
@@ -0,0 +1,11 @@
+const require = (await import('node:module')).createRequire(import.meta.url); const __chunk_filename = (await import('node:url')).fileURLToPath(import.meta.url); const __chunk_dirname = (await import('node:path')).dirname(__chunk_filename);
+import {
+  import_dist,
+  init_https_proxy_agent
+} from "./chunk-TUDYL3X4.js";
+import "./chunk-34MYV7JD.js";
+init_https_proxy_agent();
+var export_HttpsProxyAgent = import_dist.HttpsProxyAgent;
+export {
+  export_HttpsProxyAgent as HttpsProxyAgent
+};
